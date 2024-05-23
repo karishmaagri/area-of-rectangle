69d5cf381cec3fa0f76abd3bc65c0c5b10b93455
karishmaagri <169182736+karishmaagri@users.noreply.github.com>
2024-05-24 00:11:33 +0530

    Add files via upload

---
+++ Demoemployee.java
@@ -0,0 +1,23 @@
+package employee;
+
+public class Demoemployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+emppersonalinfo epr = new emppersonalinfo();
+
+epr.setEname("Karishma");
+epr.setEmailId("K@gmail.com");
+epr.setMobileno(12345);
+
+System.out.println(epr);
+
+emppersonalinfo epr1 = new emppersonalinfo();
+epr1.setEname("Kaju");
+
+
+System.out.println(epr1);
+
+	}
+
+}
