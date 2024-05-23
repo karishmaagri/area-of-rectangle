cdd2921855fa4a999d9f4aca22c052814fb0976b
karishmaagri <169182736+karishmaagri@users.noreply.github.com>
2024-05-24 00:11:08 +0530

    Add files via upload

---
+++ Demoemp.java
@@ -0,0 +1,15 @@
+package employee;
+
+public class Demoemp {
+
+	public static void main(String[] args) {
+      employeedata ed = new employeedata();
+      ed.setName("Karishma");
+      ed.setDesgination("Software Engineer");
+      ed.setBasicsalary(50000);
+      
+      System.out.println(ed);
+
+	}
+
+}
